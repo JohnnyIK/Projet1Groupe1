628a995cfcafae2bf6137ae86a7b45ecd5668669
Abigael Castillo <abigael.castillo@live.fr>
2022-12-02 15:57:55 +0100

    AddClassAdministrateur

---
+++ projet1Groupe1/src/main/java/fr/isika/cda22/projet1groupe1/projet1Groupe1/Administrateur.java
@@ -0,0 +1,52 @@
+package fr.isika.cda22.projet1groupe1.projet1Groupe1;
+
+public class Administrateur {
+
+		//Attributs 
+		protected String loginAdmin; 
+		protected String mdpAdmin;
+		
+		
+		//Constructeur
+		public Administrateur(String loginAdmin, String mdpAdmin) {
+			super();
+			this.loginAdmin = loginAdmin;
+			this.mdpAdmin = mdpAdmin;
+		}
+
+		//Getters & Setters
+		public String getLoginAdmin() {
+			return loginAdmin;
+		}
+
+		public void setLoginAdmin(String loginAdmin) {
+			this.loginAdmin = loginAdmin;
+		}
+
+		public String getMdpAdmin() {
+			return mdpAdmin;
+		}
+
+		public void setMdpAdmin(String mdpAdmin) {
+			this.mdpAdmin = mdpAdmin;
+		}
+		
+		//Méthode spécifique à l'administrateur
+		public void ModifStagiaire() {
+			
+		}
+		
+		public void supprimerStagiaire() {
+			
+		}
+		
+		public void ImportAnnuaire() {
+			
+		}
+		
+		
+		
+		
+	
+
+}
