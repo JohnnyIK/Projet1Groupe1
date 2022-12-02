1c93d088a3e67f86835d9e382325a6d1fae57590
JohnnyIK <johnnybutin@yahoo.fr>
2022-12-02 11:11:49 +0100

    creation fichier maven

---
+++ projet1Groupe1/src/main/java/fr/isika/cda22/projet1groupe1/projet1Groupe1/SystemInfo.java
@@ -0,0 +1,13 @@
+package fr.isika.cda22.projet1groupe1.projet1Groupe1;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ projet1Groupe1/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module fr.isika.cda22.projet1groupe1.projet1Groupe1 {
+    requires javafx.controls;
+    exports fr.isika.cda22.projet1groupe1.projet1Groupe1;
+}
