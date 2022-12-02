8285271afa9e38926315b179c506f333a23db02a
Abigael Castillo <abigael.castillo@live.fr>
2022-12-02 16:08:01 +0100

    App.java

---
+++ projet1Groupe1/src/main/java/fr/isika/cda22/projet1groupe1/projet1Groupe1/Sauvegarde.java
@@ -0,0 +1,12 @@
+package fr.isika.cda22.projet1groupe1.projet1Groupe1;
+
+public class Sauvegarde {
+
+	public void RecuperationFichierBinaire() {
+		
+	}
+	
+	public void SauvegardeFichierBinaire() {
+		
+	}
+}
